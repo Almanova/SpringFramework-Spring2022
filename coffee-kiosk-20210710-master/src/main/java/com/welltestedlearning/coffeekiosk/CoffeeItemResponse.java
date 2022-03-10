405057855c29ec626fe2a27239649b9d98343be6
madina <almanovamadina@1fit.app>
2022-03-10 23:16:30 +0600

    lab6 added

---
+++ coffee-kiosk-20210710-master/src/main/java/com/welltestedlearning/coffeekiosk/CoffeeItemResponse.java
@@ -0,0 +1,66 @@
+package com.welltestedlearning.coffeekiosk;
+
+public class CoffeeItemResponse {
+    private long id;
+    private String size;
+    private String kind;
+    private String creamer;
+    private String price;
+
+    public CoffeeItemResponse() {
+
+    }
+
+    public CoffeeItemResponse(long id, String size, String kind, String creamer, String price) {
+        this.id = id;
+        this.size = size;
+        this.kind = kind;
+        this.creamer = creamer;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getCreamer() {
+        return creamer;
+    }
+
+    public void setCreamer(String creamer) {
+        this.creamer = creamer;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public static CoffeeItemResponse from(CoffeeItem coffeeItem) {
+        return new CoffeeItemResponse(coffeeItem.getId(), coffeeItem.size(),
+                coffeeItem.kind(), coffeeItem.creamer(), "20.34");
+    }
+}
