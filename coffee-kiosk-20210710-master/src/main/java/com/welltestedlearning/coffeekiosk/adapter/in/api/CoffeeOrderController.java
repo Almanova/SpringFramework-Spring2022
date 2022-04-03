b7e2b847aae131531f84d8eceedf90c507b4436f
madina <almanovamadina@1fit.app>
2022-04-03 21:51:37 +0600

    lab 8 added

--- coffee-kiosk-20210710-master/src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderController.java
+++ coffee-kiosk-20210710-master/src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderController.java
@@ -2,17 +2,30 @@ package com.welltestedlearning.coffeekiosk.adapter.in.api;
 
 import com.welltestedlearning.coffeekiosk.domain.CoffeeItem;
 import com.welltestedlearning.coffeekiosk.domain.CoffeeItemResponse;
+import com.welltestedlearning.coffeekiosk.domain.CoffeeOrder;
+import com.welltestedlearning.coffeekiosk.domain.CoffeeOrderRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
 public class CoffeeOrderController {
+    private final CoffeeOrderRepository coffeeOrderRepository;
 
-    @GetMapping(value = "/api/coffee/order")
-    public CoffeeItemResponse getCoffeeItem() {
-        CoffeeItem coffeeItem = new CoffeeItem("small", "latte", "milk");
-        coffeeItem.setId(1L);
-        return CoffeeItemResponse.from(coffeeItem);
+    public CoffeeOrderController(CoffeeOrderRepository coffeeOrderRepository) {
+        this.coffeeOrderRepository = coffeeOrderRepository;
+    }
+
+    @GetMapping("/api/coffee/orders/{id}")
+    public ResponseEntity<CoffeeOrderResponse> coffeeOrder(@PathVariable("id") long orderId) {
+        if (orderId < 0)
+            throw new IllegalArgumentException();
+        Optional<CoffeeOrder> coffeeOrderOptional = coffeeOrderRepository.findById(orderId);
+        return coffeeOrderOptional.map(coffeeOrder -> ResponseEntity
+                .ok(CoffeeOrderResponse.from(coffeeOrder))).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- coffee-kiosk-20210710-master/src/test/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderWebTest.java
+++ coffee-kiosk-20210710-master/src/test/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderWebTest.java
@@ -1,70 +1,98 @@
 package com.welltestedlearning.coffeekiosk.adapter.in.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.welltestedlearning.coffeekiosk.CoffeeOrderRepositoryConfig;
+import com.welltestedlearning.coffeekiosk.SampleDataLoader;
+import com.welltestedlearning.coffeekiosk.CoffeeOrderRepositoryConfig;
+import com.welltestedlearning.coffeekiosk.SampleDataLoader;
 import com.welltestedlearning.coffeekiosk.domain.CoffeeItem;
-import com.welltestedlearning.coffeekiosk.domain.CoffeeItemResponse;
+import com.welltestedlearning.coffeekiosk.domain.CoffeeOrder;
+import com.welltestedlearning.coffeekiosk.domain.CoffeeOrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CoffeeOrderController.class)
-public class CoffeeOrderWebTest {
-    // in tests, it's OK to do "field" Autowiring (injection)
+class CoffeeOrderWebTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
     @Autowired
     ObjectMapper objectMapper;
 
-    @Autowired
-    private MockMvc mockMvc;
+    @MockBean
+    CoffeeOrderRepository coffeeOrderRepository;
+
+    @BeforeEach
+    public void initRepo() {
+        CoffeeItem coffeeItem = new CoffeeItem("small", "latte", "milk");
+        coffeeItem.setId(99L);
+        CoffeeOrder coffeeOrder = new CoffeeOrder("Ted", LocalDateTime.of(2020, 10, 11, 12, 13));
+        coffeeOrder.add(coffeeItem);
+        coffeeOrder.setId(23L);
+        org.mockito.Mockito.when(coffeeOrderRepository.findById(23L)).thenReturn(Optional.of(coffeeOrder));
+    }
 
     @Test
-    public void getFromCoffeeOrderEndpointIs200Ok() throws Exception {
-        mockMvc.perform(get("/api/coffee/order")
-                .accept(MediaType.APPLICATION_JSON))
+    public void getCoffeeOrderIsOk() throws Exception {
+        mockMvc.perform(get("/api/coffee/orders/23")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void getCoffeeOrderReturnsCompleteJson() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/api/coffee/order")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(content()
-                        .contentType(MediaType.APPLICATION_JSON))
+    public void getCoffeeOrderIsCompleteJson() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/api/coffee/orders/23")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn();
 
-        CoffeeItem coffeeItem = new CoffeeItem("small", "latte", "milk");
-        coffeeItem.setId(1L);
-
-        CoffeeItemResponse expectedResponse = CoffeeItemResponse.from(coffeeItem);
+        CoffeeOrder coffeeOrder = createCoffeeOrderWithOneItemAndIdOf(23L);
+        CoffeeOrderResponse expectedResponse = CoffeeOrderResponse.from(coffeeOrder);
         String expectedJson = objectMapper.writeValueAsString(expectedResponse);
 
-        assertThat(mvcResult.getResponse()
-                .getContentAsString())
+        assertThat(mvcResult.getResponse().getContentAsString())
                 .isEqualTo(expectedJson);
     }
 
-    @Test
-    public void getCoffeeOrderAsXmlIsOk() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/api/coffee/order")
-                .accept(MediaType.APPLICATION_XML))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        String xmlContent = mvcResult.getResponse().getContentAsString();
-        assertThat(xmlContent)
-                .contains("<CoffeeItemResponse>",
-                        "<id>1</id>",
-                        "<kind>latte</kind>",
-                        "<creamer>milk</creamer>",
-                        "<size>small</size>",
-                        "<price>$1</price>",
-                        "</CoffeeItemResponse>");
+    private CoffeeOrder createCoffeeOrderWithOneItemAndIdOf(long coffeeOrderId) {
+        CoffeeItem coffeeItem = new CoffeeItem("small", "latte", "milk");
+        coffeeItem.setId(99L);
+        CoffeeOrder coffeeOrder = new CoffeeOrder("Ted", LocalDateTime.of(2020, 10, 11, 12, 13));
+        coffeeOrder.add(coffeeItem);
+        coffeeOrder.setId(coffeeOrderId);
+        return coffeeOrder;
     }
+
+    @Test
+    public void getNonExistentCoffeeOrderReturnsNotFoundStatus() throws Exception {
+        mockMvc.perform(get("/api/coffee/orders/9999")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getNegativeIdReturnsBadRequestStatus() throws Exception {
+        mockMvc.perform(get("/api/coffee/orders/-1")
+            .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
 }
