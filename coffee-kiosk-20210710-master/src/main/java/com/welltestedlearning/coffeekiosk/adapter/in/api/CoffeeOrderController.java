f94d2c400869f448337a774806e915f4c2592871
madina <almanovamadina@1fit.app>
2022-04-06 00:11:31 +0600

    lab9 added

--- coffee-kiosk-20210710-master/src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderController.java
+++ coffee-kiosk-20210710-master/src/main/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderController.java
@@ -4,17 +4,22 @@ import com.welltestedlearning.coffeekiosk.domain.CoffeeItem;
 import com.welltestedlearning.coffeekiosk.domain.CoffeeItemResponse;
 import com.welltestedlearning.coffeekiosk.domain.CoffeeOrder;
 import com.welltestedlearning.coffeekiosk.domain.CoffeeOrderRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @RestController
 public class CoffeeOrderController {
     private final CoffeeOrderRepository coffeeOrderRepository;
+
+    @Value("${order.price.currency.prefix}")
+    private String currencyPrefix;
 
     public CoffeeOrderController(CoffeeOrderRepository coffeeOrderRepository) {
         this.coffeeOrderRepository = coffeeOrderRepository;
@@ -26,6 +31,20 @@ public class CoffeeOrderController {
             throw new IllegalArgumentException();
         Optional<CoffeeOrder> coffeeOrderOptional = coffeeOrderRepository.findById(orderId);
         return coffeeOrderOptional.map(coffeeOrder -> ResponseEntity
-                .ok(CoffeeOrderResponse.from(coffeeOrder))).orElseGet(() -> ResponseEntity.notFound().build());
+                .ok(CoffeeOrderResponse.from(coffeeOrder, currencyPrefix)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("api/coffee/orders")
+    public ResponseEntity createCoffeeOrder(@RequestBody CoffeeOrderRequest coffeeOrderRequest) {
+        CoffeeOrder coffeeOrder = new CoffeeOrder(coffeeOrderRequest.getCustomerName(),
+                LocalDateTime.of(2022, 4, 5, 12, 13));
+        CoffeeItem coffeeItem = new CoffeeItem(coffeeOrderRequest.getSize(), coffeeOrderRequest.getKind(),
+                coffeeOrderRequest.getCreamer());
+        coffeeOrder.add(coffeeItem);
+        CoffeeOrder savedCoffeeOrder = coffeeOrderRepository.save(coffeeOrder);
+        return ResponseEntity.created(
+                URI.create("/api/coffee/orders/" + savedCoffeeOrder.getId()))
+                .build();
     }
 }
--- coffee-kiosk-20210710-master/src/test/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderWebTest.java
+++ coffee-kiosk-20210710-master/src/test/java/com/welltestedlearning/coffeekiosk/adapter/in/api/CoffeeOrderWebTest.java
@@ -65,7 +65,7 @@ class CoffeeOrderWebTest {
                 .andReturn();
 
         CoffeeOrder coffeeOrder = createCoffeeOrderWithOneItemAndIdOf(23L);
-        CoffeeOrderResponse expectedResponse = CoffeeOrderResponse.from(coffeeOrder);
+        CoffeeOrderResponse expectedResponse = CoffeeOrderResponse.from(coffeeOrder, "US$");
         String expectedJson = objectMapper.writeValueAsString(expectedResponse);
 
         assertThat(mvcResult.getResponse().getContentAsString())
